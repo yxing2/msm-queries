444c24e1c8c6d90ec9c459953bb536cf3e5b2487
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-08-03 00:43:48 +0000

    Directors show movies below

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,7 +7,6 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("the_id")
     matching_records = Director.where({:id => the_id})
     @the_director = matching_records.at(0)
-
     render({:template => "director_templates/details"})
   end
 end
