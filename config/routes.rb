3cf5fe312f733d6079046eee60b7e0b34aa7a17d
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-08-05 17:44:25 +0000

    latest changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
   get("/directors", {:controller => "directors", :action => "index"})
   get("/directors/:the_id", {:controller => "directors", :action => "show"})
 
   get("/movies", {:controller => "movies", :action => "index"})
   get("/movies/:the_id", {:controller => "movies", :action => "show"})
+
+  get("/actors", {:controller => "actors", :action => "index"})
+  get("/actors/:the_id", {:controller => "actors", :action => "show"})
+
 end
